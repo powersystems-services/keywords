34fe1e75c1190b903b6ea22290441621ffefe305
Eli <samson.tan@obf.ateneo.edu>
2023-04-28 14:54:18 +0800

    edit script.js

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ let strDocument
 import { EN_Keywords_E_mobility, EN_Keywords_Power_Systems, EN_Keywords_Services, EN_Keywords_Utilities, CN_Keywords_E_mobility, CN_Keywords_Power_Systems, CN_Keywords_Services, CN_Keywords_Utilities, KR_Keywords_E_mobility, KR_Keywords_Power_Systems, KR_Keywords_Services, KR_Keywords_Utilities, DE_Keywords_E_mobility, DE_Keywords_Power_Systems, DE_Keywords_Services, DE_Keywords_Utilities, ES_Keywords_E_mobility, ES_Keywords_Power_Systems, ES_Keywords_Services, ES_Keywords_Utilities, FR_Keywords_E_mobility, FR_Keywords_Power_Systems, FR_Keywords_Services, FR_Keywords_Utilities } from "./keywords.js";
 
 
-let languageIndex, languageText, categoryIndex, categoryText 
+let languageIndex, languageText, languageValue, categoryIndex, categoryText
 let combinedKeywords = [[EN_Keywords_Power_Systems, EN_Keywords_Services , EN_Keywords_Utilities, EN_Keywords_E_mobility],[CN_Keywords_Power_Systems, CN_Keywords_Services,CN_Keywords_Utilities, CN_Keywords_E_mobility], [KR_Keywords_Power_Systems, KR_Keywords_Services , KR_Keywords_Utilities, KR_Keywords_E_mobility], [DE_Keywords_Power_Systems, DE_Keywords_Services,DE_Keywords_Utilities, DE_Keywords_E_mobility], [ES_Keywords_Power_Systems,ES_Keywords_Services,ES_Keywords_Utilities, ES_Keywords_E_mobility], [FR_Keywords_Power_Systems, FR_Keywords_Services , FR_Keywords_Utilities, FR_Keywords_E_mobility]]
-
 //EN, CN, KR, DE, ES, FR
 // Power Systems, Services, Utilities, E-mobility 
 
@@ -18,10 +17,24 @@ function setParameters() {
     var getLanguage = document.getElementById('language');
     languageIndex = getLanguage.options[getLanguage.selectedIndex].index;
     languageText = getLanguage.options[getLanguage.selectedIndex].text;
+    languageValue = getLanguage.options[getLanguage.selectedIndex].value; 
 
     var getCategory = document.getElementById('category');
     categoryIndex = getCategory.options[getCategory.selectedIndex].index;
     categoryText = getCategory.options[getCategory.selectedIndex].text; 
+}
+
+
+function getWordCount(text) {
+
+    switch(languageValue) {
+
+        case "CN":
+        case "KR":
+            return text.length - text.split(" ").length + 1;
+        default:
+            return text.split(" ").length;
+    }
 }
 
 
@@ -54,9 +67,9 @@ btnSubmit.addEventListener("click", function(event){
     arr.forEach(function(i) {
         let re
 
-        switch(languageIndex){
-            case 1 :
-            case 2 :
+        switch(languageValue){
+            case "CN" : 
+            case "KR" :
                 re = new RegExp( i, 'gi')
                 break; 
             default:
@@ -78,7 +91,6 @@ btnSubmit.addEventListener("click", function(event){
 
     let p = document.createElement("p")
     p.innerText = "--RESULTS--"
-    p.style.fontWeight = "bold"
     document.getElementById('results').append(p)
     //store results in a dict then loop through dict to get the results
 
@@ -93,19 +105,22 @@ btnSubmit.addEventListener("click", function(event){
 
     let p1 = document.createElement("p")
     let p2 = document.createElement("p")
+    let p3 = document.createElement("p")
+    let p4 = document.createElement("p")
 
+    let wordCount = getWordCount(textBox.value)
+    
     p1.innerText = "Total Number of Keywords: " + Object.keys(resultDict).length 
     p2.innerText = "Total Number of Mentions: " + counter
+    p3.innerText = "Word Count: " + wordCount   
+    p4.innerText = "Keyword Percentage: " + counter/wordCount * 100 + "%"
 
-    p1.style.fontWeight = "bold"
-    p2.style.fontWeight = "bold"
     
     document.getElementById('results').append("----")
     document.getElementById('results').append(p1)
     document.getElementById('results').append(p2)
-
-
-
+    document.getElementById('results').append(p3)
+    document.getElementById('results').append(p4)
 })
 
 btnClear.addEventListener("click", function(event){
@@ -114,7 +129,6 @@ btnClear.addEventListener("click", function(event){
     while (resultsDiv.hasChildNodes()) {
         resultsDiv.removeChild(resultsDiv.firstChild);
     }
-
 })
 
 window.addEventListener("keydown", function(event) {
@@ -122,6 +136,7 @@ window.addEventListener("keydown", function(event) {
         btnSubmit.click()
     }
 })
+
 
 
 
